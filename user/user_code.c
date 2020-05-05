c8a8cecbdc790e42e516cd09f3439a548033975c
Huang-Junjie <1582695658@qq.com>
2020-05-05 22:44:46 +0800

    update

--- user/user_code.c
+++ user/user_code.c
@@ -1,19 +1,11 @@
 #include <printf.h>
 
 int main() {
-  int n, pid;
-  for (n = 0; n < 5; n++) {
-    if ((pid = fork()) == 0) {
-      printf("I am child %d\n", n);
+    int pid = fork();
+    if (pid == 0) {
+        printf("user: child\n");
+    }else {
+        printf("user: parent\n");
     }
-    assert(pid > 0);
-  }
-
-  for (; n > 0; n--) {
-    assert(wait() == 0);
-  }
-
-  assert(wait() != 0);
-  printf("I am parent %d\n", n);
-  return 0;
+    return 0;
 }
