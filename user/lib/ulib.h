89045d06ab6a648817ad965a83408ac95db242d9
Huang-Junjie <1582695658@qq.com>
2020-05-05 22:41:23 +0800

    update

--- user/lib/ulib.h
+++ user/lib/ulib.h
@@ -1,14 +1,6 @@
 #ifndef _ULIB_H_
 #define _ULIB_H_
 
-#define assert(x)                        \
-  do {                                   \
-    if (!(x)) {                          \
-      panic("assertion failed: %s", #x); \
-    }                                    \
-  } while (0)
-
-
 void exit(int error_code);
 int fork(void);
 int wait(void);
